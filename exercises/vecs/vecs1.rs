22410cccb22f07e6951b755b4be79f60d14cd65d
wrinkled <wrinkledeth@gmail.com>
2022-09-02 21:27:10 -0400

    current progress

--- exercises/vecs/vecs1.rs
+++ exercises/vecs/vecs1.rs
@@ -4,11 +4,26 @@
 // Make me compile and pass the test!
 // Execute `rustlings hint vecs1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40]; // a plain array
-    let v = // TODO: declare your vector here with the macro for vectors
+
+    // TODO: declare your vector here with the macro for vectors
+    // Method 1: Builtin
+    let v = a.to_vec();
+
+    // Method 2: Build Vec with array
+    let v = vec![10, 20, 30, 40];
+
+    // Method 4: Iterate through array and push to vec
+    let mut v: Vec<i32> = Vec::new();
+    for i in &a {
+        v.push(*i);
+    }
+
+    // t Method 3: Build Vec with existing awway
+    // let v = vec![a];
+
+    // println!("{:?}", v);
 
     (a, v)
 }
